ae4010034d3785e0fc9bd5fc4e83721104665864
pixelzerg <pixelzerg@gmail.com>
2021-01-11 00:25:37 +0000

    implemented own unloading for increased performance (because the website didn't implement it themselves :D)

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+import itertools
 import time
 from datetime import datetime
 from selenium import webdriver
@@ -11,12 +12,18 @@ import re
 import json
 import os
 
-DOWNLOAD_PATH = os.path.expanduser("~/Downloads/sams")
-DATA_TEST_IMAGE = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWBAMAAADOL2zRAAAAG1BMVEXMzMyWlpaqqqq3t7fFxcW+vr6xsbGjo6OcnJyLKnDGAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABAElEQVRoge3SMW+DMBiE4YsxJqMJtHOTITPeOsLQnaodGImEUMZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXmXLy2jEbiqE6Df7DTleA5socLqvEFVxtJyrpZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
+CACHE_PATH = "cache"
+BATCH_SIZE = 40
+UNLOAD = True
+CHECK_ONCE_EVERY = 50
 
+os.makedirs(CACHE_PATH, exist_ok=True)
+options = webdriver.ChromeOptions()
+options.add_argument(r"--user-data-dir={}".format(os.path.abspath(CACHE_PATH)))
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://support.samsungcloud.com/")
+
 input("Please sign in and navigate to the gallery page.\n"
       "Note: you may want to change the download folder in Chrome as well.\n"
       "Press enter here once you are done.")
@@ -29,22 +36,37 @@ def _scroll_to_if_needed(elem):
 def _scroll_to_smooth(elem):
     driver.execute_script(r"arguments[0].scrollIntoView({behavior:'smooth'});", elem)
 
-def get_listitem_by_index(index_):
+
+def _remove_from_dom(elem):
+    driver.execute_script(r"arguments[0].remove();", elem)
+
+
+def get_listitem_by_index(index_, unload=True, max_items=160):
     """
     index is computed using the tags themselves
     (accounts for if the counting in the DOM is not contiguous
     in practice, the images can be assumed to be in-order, persistent in the DOM and contiguous,
-    and so get_listitem_by_index_fast may be used)
+    and so get_lixstitem_by_index_fast may be used)
     """
     index = index_ + 1
     last_computed_index = 0
     last_scrolled = 0
     while True:
-        # calculate dict
         sec_psum = 0
         last_sec = 0
         last_num = 0
-        for listitem in driver.find_elements_by_xpath("//div[@role='listitem']"):
+        listitems = driver.find_elements_by_xpath("//div[@role='listitem']")
+
+        if unload:
+            # keep last `max_items` listitems only
+            count = 0
+            for listitem in listitems[:-max_items]:
+                _remove_from_dom(listitem)
+                count += 1
+            print("Unloaded {}".format(count))
+            listitems = listitems[-max_items:]
+
+        for listitem in listitems:
             sec, num = map(int, re.match(r"gallerySelector(\d+)_(\d+)", listitem.get_attribute("id")).groups())
 
             if sec > last_sec:
@@ -64,12 +86,19 @@ def get_listitem_by_index(index_):
                 _scroll_to_smooth(listitem)
                 last_scrolled = last_computed_index
 
-def get_listitem_by_index_fast(index):
+
+_unload_offset = 0
+
+
+def get_listitem_by_index_fast(index, max_items=105):
+    global _unload_offset
+
     last_len = 0
     no_change_count = 0  # number of times count has consecutively been the same
     while True:
         listitems = driver.find_elements_by_xpath("//div[@role='listitem']")
-        cur_len = len(listitems)
+
+        cur_len = _unload_offset + len(listitems)
         if index >= cur_len:
             if cur_len != last_len:
                 _scroll_to_smooth(listitems[-1])
@@ -82,7 +111,8 @@ def get_listitem_by_index_fast(index):
                     raise RuntimeError
             time.sleep(0.4)
         else:
-            return listitems[index]
+            return listitems[index - _unload_offset]
+
 
 def select_listitem(listitem):
     # hover over listitem
@@ -96,6 +126,7 @@ def select_listitem(listitem):
     ))
     check_circle.click()
 
+
 def select_range(start_index, n):
     """
     :return: successfully selected range without truncating or not
@@ -107,6 +138,7 @@ def select_range(start_index, n):
         except RuntimeError:
             return False
     return True
+
 
 def download_selected(wait_for_toast_fade=True):
     fail_count = 1
@@ -141,7 +173,11 @@ def download_selected(wait_for_toast_fade=True):
             if fail_count > 3:
                 raise
 
-def download(start_index=0, batch_size=40, only_one_batch=False):
+
+def download(start_index=0, batch_size=40, only_one_batch=False, reload=True):
+    # check_gen = itertools.count(0, CHECK_ONCE_EVERY)
+    # next_check = next(check_gen)
+
     cur_index = start_index
     last_index = start_index
     while True:
@@ -151,6 +187,8 @@ def download(start_index=0, batch_size=40, only_one_batch=False):
             success = select_range(cur_index, batch_size)
             download_selected()
 
+            unload()
+
             if not success:
                 print("(Hopefully) reached end!")
                 break
@@ -159,12 +197,18 @@ def download(start_index=0, batch_size=40, only_one_batch=False):
                     return
                 cur_index += batch_size
         except:
-            print("Fatal error! Reloading and retrying in a bit...")
-            time.sleep(5)
-            driver.get("https://support.samsungcloud.com/#/gallery")
+            if reload:
+                print("Fatal error! Reloading and retrying in a bit...")
+                time.sleep(5)
+                driver.get("https://support.samsungcloud.com/#/gallery")
+                global _unload_offset
+                _unload_offset = 0
+            else:
+                continue
         finally:
             print("{} -- Downloaded {}-{}".format(datetime.utcnow().strftime("%x %X"), last_index, cur_index))
             last_index = cur_index
+
 
 def _download_file(name, url):
     driver.execute_script("""
@@ -180,12 +224,30 @@ def _download_file(name, url):
     delete link;
     """, name, url)
 
+
+DATA_TEST_IMAGE = "data:image/png;base64,iVBORw0KGV9Jm2u7rmsCe65wKzPTw5jtS38n2tVEGiDtbLrcW77HPEwrJM2Ej2yFNYwNQCeaxcW+vr6xsbGV9Jm2u7rmsCe65wKzPTw5jtS38n2tVEGiDtbLrcW77HPEwrJM2Ej2yFNYwN+DLgjH2m5c8emE66pjdExmgep47BAdKTrCJ7ZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXnHiSgDznyvEWLRDTty2Ej8fUgrwm3Xjt1CJ7ZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
+
+
 def _download_multiple_prompt(n=5):
     for i in range(n):
         _download_file("test", DATA_TEST_IMAGE)
     input("Please allow downloading multiple files. Press enter here once done")
 
-def download_thumbnails(start_index=0, download_once_every=50):
+
+def unload():
+    if UNLOAD:
+        global _unload_offset
+        listitems = driver.find_elements_by_xpath("//div[@role='listitem']")
+        # keep last `max_items` listitems only
+        for listitem in listitems[:-105]:
+            _remove_from_dom(listitem)
+            _unload_offset += 1
+        # listitems = listitems[-max_items:]
+        time.sleep(2)
+        print("Unload offset now: {}".format(_unload_offset))
+
+
+def download_thumbnails(start_index=0, reload=True):
     _download_multiple_prompt()
 
     cur_index = start_index
@@ -194,8 +256,9 @@ def download_thumbnails(start_index=0, download_once_every=50):
         try:
             fail_count = 0
             while True:
-                if cur_index % download_once_every == 0:
+                if cur_index % CHECK_ONCE_EVERY == 0:
                     download(cur_index, 2, True)
+                    unload()
 
                 try:
                     print("{}: Trying to download thumb #{:05d}".format(datetime.utcnow().strftime("%x %X"), cur_index))
@@ -220,13 +283,19 @@ def download_thumbnails(start_index=0, download_once_every=50):
                     traceback.print_exc()
                     time.sleep(5)
                     fail_count += 1
-                    if fail_count > 5:
-                        print("{}: Fatal error! Reloading and retrying in a bit...".format(
-                            datetime.utcnow().strftime("%x %X")))
-                        fail_count = 0
-                        time.sleep(5)
-                        driver.get("https://support.samsungcloud.com/#/gallery")
+                    if reload:
+                        if fail_count > 5:
+                            print("{}: Fatal error! Reloading and retrying in a bit...".format(
+                                datetime.utcnow().strftime("%x %X")))
+                            fail_count = 0
+                            time.sleep(5)
+                            driver.get("https://support.samsungcloud.com/#/gallery")
+                            global _unload_offset
+                            _unload_offset = 0
         except RuntimeError:
             print("End reached!")
             break
-download_thumbnails(start_index=700)
+
+
+# download_thumbnails(start_index=0)
+download(start_index=0)
